9de3940d7d5e5df7688aac56b1cbad1f219cbc3d
Nitesh Singh <ni30singh420@gmail.com>
2024-06-06 08:36:51 +0530

    make selenium work on headless modee

---
+++ Read Me.md
@@ -0,0 +1,11 @@
+# Twitter Trending
+
+```powershell
+pip install -r requirements.txt
+```
+
+```powershell
+python index.py
+```
+
+URL : http://127.0.0.1:5000/
--- scrap_twitter.py
+++ scrap_twitter.py
@@ -1,27 +1,32 @@
+import random 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import random
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 PROXY = ["213.157.243.196:5678", "122.136.212.132:53281",
          "185.162.231.178:80", "70.185.95.177:39593"]
 i = random.randint(0, 3)
 
+
 chrome_options = Options()
-chrome_options.add_experimental_option(
-    "detach", True)
-chrome_options.add_argument('--disk-cache-dir=/path/to/cache')
-# chrome_options.add_argument('--proxy-server=%s' % PROXY[i])
+chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless=new')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument('log-level=3')
 
 
 # Get IP
 
 def get_ip():
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get('https://api.ipify.org/')
     ip = driver.find_element(By.TAG_NAME, "body").text
     driver.close()
@@ -34,15 +39,19 @@ start_time_wait = 30
 # Login Twitter
 
 def login_twitter():
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome()
     driver.get("https://x.com/i/flow/login")
     wait = WebDriverWait(driver, start_time_wait)
     wait.until(EC.presence_of_element_located(
         (By.XPATH, "//input[@name='text']")))
+    
+    
     input_email = driver.find_element(By.XPATH, "//input[@name='text']")
     input_email.send_keys("vinningrazors329@gmail.com")
     wait.until(EC.element_to_be_clickable(
         (By.XPATH, "//button[@role='button']")))
+    
+    
     next_btn = driver.find_elements(By.XPATH, "//button[@role='button']")
     for i in next_btn:
         try:
@@ -53,11 +62,11 @@ def login_twitter():
         except Exception:
             continue
 
-    wait.until(EC.presence_of_element_located(
-        (By.XPATH, "//input[@data-testid='ocfEnterTextTextInput']")))
-    input_username = driver.find_element(
-        By.XPATH, "//input[@data-testid='ocfEnterTextTextInput']")
-    input_username.send_keys("@Winner949124"+Keys.RETURN)
+    # wait.until(EC.presence_of_element_located(
+    #     (By.XPATH, "//input[@data-testid='ocfEnterTextTextInput']")))
+    # input_username = driver.find_element(
+    #     By.XPATH, "//input[@data-testid='ocfEnterTextTextInput']")
+    # input_username.send_keys("@Winner949124"+Keys.RETURN)
 
     wait.until(EC.presence_of_element_located(
         (By.XPATH, "//input[@type='password']")))
@@ -84,6 +93,11 @@ def get_trends(driver):
 
     final_data = [i for i in new_trend if not any(x in i for x in [
         "posts", "Trending in India", "What’s happening", '· Trending', "Show more"])]
-    
+
     driver.close()
     return final_data
+
+
+if __name__ == "__main__":
+    print(get_ip())
+    print(get_trends(login_twitter()))
